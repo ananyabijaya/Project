2c5882ee08ccdc8f8552abd436fe70035366b998
Ananya <ananyabijaya7@gmail.com>
2018-10-29 21:55:00 +0530

    Update ExplicitPDE_sol.m

--- ExplicitPDE_sol.m
+++ ExplicitPDE_sol.m
@@ -1,5 +1,6 @@
 %% Numerical modelling and simulation project
 %Dynamic response of a simply supported beam subjected to a concentrated load at the center
+%Change made in the implicit branch
 clc
 clear
 %Given
